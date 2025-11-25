39363cb9991e607e23bd90da0e316416e4c7f410
RiccardoFermi <riccardofermi9@gmail.com>
2025-11-25 10:11:27 +0100

    Enable Vercel Analytics 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "PL&CO",
